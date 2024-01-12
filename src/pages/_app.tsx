9b453dd04d94895d68bf99a25c21fa4a45712039
Gustavo Morais Lopes dos Santos <gustavo.morais1414@gmail.com>
2024-01-12 07:55:57 -0300

    fiix: alteração da linguagem do provider para PT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { JobProvider } from "@/context/JobContext";
+import pt_BR from 'antd/locale/pt_BR';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <JobProvider>
-      <ConfigProvider>
+      <ConfigProvider locale={pt_BR}>
         <Component {...pageProps} />
       </ConfigProvider>
   </JobProvider>
